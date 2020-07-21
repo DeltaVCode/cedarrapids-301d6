40560c40705132109fa6bc423c5f8753e1135c84
Marc Hauschildt <marc.hauschildt@gmail.com>
2020-07-21 17:09:33 -0500

    Lab 2 demo

---
+++ class-02/demo/read-json/scripts/app.js
@@ -0,0 +1,48 @@
+'use strict';
+
+$(() => {
+  const ajaxSettings = { method: 'get', dataType: 'json' };
+  $.ajax('./data/people.json', ajaxSettings)
+    .then((data) => {
+      const arrayOfPeople = data.results;
+      // console.log(arrayOfPeople);
+      arrayOfPeople.forEach((person) => {
+        Person.all.push(new Person(person));
+      });
+      // console.log(Person.all);
+    })
+    .then(() => {
+      renderPeople();
+    });
+});
+
+function Person(person) {
+  this.name = {
+    first: person.name.first,
+    last: person.name.last
+  };
+  this.image_url = person.picture.large;
+  this.location = {
+    city: person.location.city,
+    state: person.location.state
+  };
+  this.getFullName = () => `${this.name.first} ${this.name.last}`;
+}
+
+Person.all = [];
+
+Person.prototype.render = function () {
+  let $template = $('.person-template').clone();
+  $template.find('.fullName').text(this.getFullName);
+  $template.removeClass('person-template');
+  $template.find('.profileImage').attr('src', this.image_url);
+  $template.find('.profileImage').attr('alt', this.getFullName);
+  $template.find('.location').text(`${this.location.city}, ${this.location.state}`);
+  console.log($template);
+  return $template;
+};
+
+function renderPeople() {
+  Person.all.forEach(person => $('#photo-gallery').append(person.render()));
+  $('.person-template').remove();
+}
