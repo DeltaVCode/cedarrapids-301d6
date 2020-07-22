5fefbed76dfbd4d94550b66286711860b965c8b3
Marc Hauschildt <marc.hauschildt@gmail.com>
2020-07-22 15:29:12 -0500

    Posted mustache demo

--- class-02/demo/read-json/scripts/app.js
+++ class-02/demo/read-json/scripts/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 $(() => {
+  $('#photo-gallery').hide();
   const ajaxSettings = { method: 'get', dataType: 'json' };
   $.ajax('./data/people.json', ajaxSettings)
     .then((data) => {
@@ -11,6 +12,10 @@ $(() => {
     })
     .then(() => {
       renderPeople();
+      renderFilters();
+      handleFilters();
+      $('.spinner').fadeOut();
+      $('#photo-gallery').fadeIn();
     });
 });
 
@@ -25,9 +30,14 @@ function Person(person) {
     state: person.location.state
   };
   this.getFullName = () => `${this.name.first} ${this.name.last}`;
+  this.nationality = person.nat;
+  if(Person.allNationalities.indexOf(this.nationality) < 0) {
+    Person.allNationalities.push(this.nationality);
+  }
 }
 
 Person.all = [];
+Person.allNationalities = [];
 
 Person.prototype.render = function () {
   let $template = $('.person-template').clone();
@@ -36,10 +46,30 @@ Person.prototype.render = function () {
   $template.find('.profileImage').attr('src', this.image_url);
   $template.find('.profileImage').attr('alt', this.getFullName);
   $template.find('.location').text(`${this.location.city}, ${this.location.state}`);
+  $template.attr('data-nationality', this.nationality);
   return $template;
 };
 
 function renderPeople() {
   Person.all.forEach(person => $('#photo-gallery').append(person.render()));
   $('.person-template').remove();
+}
+
+function renderFilters() {
+  Person.allNationalities.sort();
+  Person.allNationalities.forEach(nationality => {
+    const $option = $('<option>').text(nationality).attr('value', nationality);
+    $('#nationality-filter').append($option);
+  });
+}
+
+function handleFilters() {
+  $('#nationality-filter').on('change', function() {
+    if($(this).val() !== '') {
+      $('.person').hide();
+      $(`.person[data-nationality*="${$(this).val()}"]`).fadeIn();
+    } else {
+      $('.person').fadeIn();
+    }
+  });
 }
--- class-03/demo/templates/scripts/app.js
+++ class-03/demo/templates/scripts/app.js
@@ -1,2 +1,46 @@
 'use strict';
 
+$(() => {
+  const ajaxSettings = { method: 'get', dataType: 'json'};
+  $.ajax('./data/dataSet.json', ajaxSettings)
+    .then((data) => {
+      const arrayOfNeighborhoods = data.neighborhoods;
+      arrayOfNeighborhoods.forEach(neighborhoodObj => {
+        Neighborhood.all.push(new Neighborhood(neighborhoodObj));
+      });
+    })
+    .then(() => {
+      renderNeighborhoods();
+    });;
+});
+
+
+function Neighborhood(obj) {
+  for(let key in obj) {
+    this[key] = obj[key];
+  }
+  this.url = `https://www.google.com/maps/place/${this.name}+Neighborhood,${this.city}`;
+  this.stars = function() {
+    let result ='';
+    let fullStar = '<i class="fa fa-star"></i>';
+    let emptyStar = '<i class="fa fa-star-o"></i>';
+    let halfStar = '<i class="fa fa-star-half-o"></i>';
+    result += fullStar.repeat(this.rating);
+    result += this.rating % 1 !== 0 ? halfStar : '';
+    result += emptyStar.repeat(5-this.rating);
+    return result + ' (' + this.rating + ')';
+  };
+}
+
+Neighborhood.all = [];
+
+Neighborhood.prototype.render = function () {
+  const templateHTML = $('#neighborhood-template').html();
+  const renderedHTML = Mustache.render(templateHTML, this);
+  return renderedHTML;
+};
+
+	
+function renderNeighborhoods() {
+  Neighborhood.all.forEach(neighborhood => $('#neighborhoods').append(neighborhood.render()));
+}
