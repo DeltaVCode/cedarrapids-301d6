02ca7fa313a61210a5decf73def0c1a82b1dfd7b
Keith Dahlby <keith@newbo.co>
2020-08-03 15:52:06 -0500

    Our first EJS view

--- class-11/demo/templating/server.js
+++ class-11/demo/templating/server.js
@@ -3,10 +3,18 @@
 const express = require('express');
 
 const app = express();
+app.set('view engine', 'ejs');
+
+app.get('/', (request, response) => {
+  let viewModel = {
+    user: { username: 'dahlbyk' }
+  }
+  response.render('index', viewModel);
+})
+
+app.get('*', (request, response) => response.status(404).send('This route does not exist'));
 
 // Use this as a talking point about environment variables
 const PORT = process.env.PORT || 3000;
-
-app.get('*', (request, response) => response.status(404).send('This route does not exist'));
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
