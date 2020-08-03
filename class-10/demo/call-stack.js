e7aea97fb12aa02e611e5e70d5e1e78b99cca6d9
Keith Dahlby <keith@newbo.co>
2020-08-03 08:56:38 -0500

    More call stack demo

--- class-10/demo/call-stack.js
+++ class-10/demo/call-stack.js
@@ -13,8 +13,30 @@ function normalize(str) {
 
 function render(stuff) {
   console.log(stuff);
+  //console.trace();
+
+  if (stuff === 'CRAIG') throw new Error('oops');
 }
 
 say(name);
 
 console.log(normalize('Rosie'));
+
+
+function roleCall() {
+  ['Keith', 'Marc', 'Craig', 'Jon'].forEach(function sayForEach(name) {
+    try {
+      say(name);
+    } catch (err) {
+      console.log(err);
+    }
+  });
+}
+
+roleCall();
+
+const sayLater = () => {
+  say('Keith is usually late');
+};
+
+setTimeout(sayLater, 5000);
