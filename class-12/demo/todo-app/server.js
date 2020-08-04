9f8324d92ac8e23a7cfdd1093eb765daa54e30fd
Keith Dahlby <keith@newbo.co>
2020-08-04 14:19:24 -0500

    Reset demo 12

--- class-12/demo/todo-app/server.js
+++ class-12/demo/todo-app/server.js
@@ -19,7 +19,6 @@ app.use(express.static('./public'));
 
 // Database Setup
 const client = new pg.Client(process.env.DATABASE_URL);
-client.connect();
 client.on('error', err => console.error(err));
 
 // Set the view engine for server-side templating
@@ -36,47 +35,24 @@ app.post('/add', addTask);
 
 app.get('*', (req, res) => res.status(404).send('This route does not exist'));
 
-app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
-
+client.connect()
+  .then(() => {
+    app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+  });
 
 // HELPER FUNCTIONS
 
 function getTasks(request, response) {
-  let SQL = 'SELECT * from tasks;';
-
-  return client.query(SQL)
-    .then(results => response.render('index', { results: results.rows }))
-    .catch(handleError);
 }
 
 function getOneTask(request, response) {
-  let SQL = 'SELECT * FROM tasks WHERE id=$1;';
-  let values = [request.params.task_id];
-
-  return client.query(SQL, values)
-    .then(result => {
-      // console.log('single', result.rows[0]);
-      return response.render('pages/detail-view', { task: result.rows[0] });
-    })
-    .catch(err => handleError(err, response));
 }
 
 function showForm(request, response) {
-  response.render('pages/add-view');
 }
 
 function addTask(request, response) {
-  console.log(request.body);
-  let { title, description, category, contact, status } = request.body;
-
-  let SQL = 'INSERT INTO tasks(title, description, category, contact, status) VALUES ($1, $2, $3, $4, $5);';
-  let values = [title, description, category, contact, status];
-
-  return client.query(SQL, values)
-    .then(response.redirect('/'))
-    .catch(err => handleError(err, response));
 }
 
 function handleError(error, response) {
-  response.render('pages/error-view', { error: 'Uh Oh' });
 }
